96cde7a83c21e11ea1ce7ec77bf16d13f062b9e1
Sai Sashreek <saisashreekd@gmail.com>
2024-04-19 12:20:31 +0530

    Quiz API Integrated with Loading State

--- screens/result.jsx
+++ screens/result.jsx
@@ -1,26 +1,43 @@
 /* eslint-disable prettier/prettier */
-import {StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
+import Title from '../components/title';
 
-const Result = ({navigation}) => {
+const Result = ({navigation, route}) => {
+  const {score} = route.params;
+
+  const scoreBanner =
+    score > 40 ? (
+      <Image
+        source={require('../assets/images/Victory.jpg')}
+        style={styles.banner}
+        resizeMode="contain"
+      />
+    ) : (
+      <Image
+        source={require('../assets/images/Failure.jpg')}
+        style={styles.banner}
+        resizeMode="contain"
+      />
+    );
   return (
     <SafeAreaView>
-      <View>
-        <View>
-          <Text>Result</Text>
-        </View>
-        <View style={styles.bannerContainer}>
-          <Image
-            source={require('../assets/images/Exams-bro.png')}
-            style={styles.banner}
-            resizeMode="contain"
-          />
-        </View>
-        <View>
-          <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-            <Text>Home</Text>
-          </TouchableOpacity>
-        </View>
+      <View style={styles.container}>
+        <Title titleText={'Results'} />
+        <Text style={styles.scoreText}>Quiz Score: {score}</Text>
+        <View style={styles.bannerContainer}>{scoreBanner}</View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Home')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>GO TO HOME</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -29,12 +46,44 @@ const Result = ({navigation}) => {
 export default Result;
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ffffff',
+    fontFamily: 'DMSans-Bold',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    height: '100%',
+  },
   bannerContainer: {
     justifyContent: 'center',
     alignItems: 'center',
+    flex: 1,
   },
   banner: {
     height: 300,
     width: 300,
+  },
+  button: {
+    width: '100%',
+    backgroundColor: '#52796F',
+    padding: 16,
+    borderRadius: 16,
+    alignItems: 'center',
+    marginBottom: 35,
+  },
+  buttonText: {
+    fontFamily: 'DMSans-Bold',
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#ffffff',
+    textTransform: 'uppercase',
+  },
+  scoreText: {
+    fontFamily: 'DMSans-Bold',
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#344E41',
+    textTransform: 'uppercase',
+    justifyContent: 'center',
+    alignSelf: 'center',
   },
 });
